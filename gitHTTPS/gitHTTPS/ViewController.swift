20ee9d30f0b35078619eb8ef81f0574016044f24
motonao-suzue <2017.onlineuse@gmail.com>
2020-05-23 16:36:23 +0900

    sample commit

---
+++ gitHTTPS/gitHTTPS/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  gitHTTPS
+//
+//  Created by 鈴江元尚 on 2020/05/23.
+//  Copyright © 2020 鈴江元尚. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("sample")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
